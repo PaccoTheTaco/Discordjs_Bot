ccbd0a8ca6d30e2a6883be74af4004d5fcd00fb1
PaccoTheTaco <153214143+PaccoTheTaco@users.noreply.github.com>
2024-07-13 12:53:04 +0200

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(TOKEN);
